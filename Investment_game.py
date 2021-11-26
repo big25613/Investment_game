68590624d91658817c2df696d57cc39c2839c48d
Gyung <gyjang@deloitte.nl>
2021-11-26 14:22:35 +0100

    stocks aantallen

--- Investment_game.py
+++ Investment_game.py
@@ -1,48 +1,78 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
-portfolio={}
+
+portfolio= pd.DataFrame()
+portfolio_per_stock=pd.DataFrame()
+
 def GetPrices():
     while True:
-        try:
-            ticker=input("Give a ticker: ")
-            response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ticker+"&apikey=demo")
+        ticker=input("Give a ticker: ")
+        response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ticker+"&apikey=G2NO055HSLUUAZWI")
 
-            # Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
-            # See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-            if response.status_code != 200:
-                raise ValueError("Could not retrieve data, code:", response.status_code)
-            raw_data = response.json()
-
-            data = raw_data['Time Series (Daily)']
-            df = pd.DataFrame(data).T.apply(pd.to_numeric)
-            price = df.iloc[:, 3]
-            portfolio[ticker]=price
-            answer = input("Want to continue [y/n]? ")
-            if answer == 'y':
-                continue
-            else:
-                break
-        except:
+                # Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
+        # See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+        if response.status_code != 200:
+            raise ValueError("Could not retrieve data, code:", response.status_code)
+        raw_data = response.json()
+        if 'Time Series (Daily)' not in raw_data:
             print("ticker not found, please try again")
+            continue
 
+        numberofstocks = int(input("How many stocks do you want to buy?"))
+        data = raw_data['Time Series (Daily)']
+        df = pd.DataFrame(data).T.apply(pd.to_numeric)
+        price_per_stock = df.iloc[:, 3]
 
+        portfolio_per_stock[ticker] = price_per_stock
+        # portfolio_per_stock['aantal']= numberofstocks
+        price = numberofstocks*price_per_stock
+        portfolio[ticker]= price
+
+        answer = input("Want to continue [y/n]? ")
+        if answer == 'y':
+            continue
+        else:
+            break
 
 
 stock_price=GetPrices()
-print(portfolio)
-print(list(portfolio.values()))
-data = pd.DataFrame(portfolio.values()).T
+print(portfolio_per_stock)
+data = pd.merge(portfolio,portfolio_per_stock, left_index=True, right_index=True)
 sortdata = data.sort_index(ascending=True)
+print(sortdata)
+
+
 sortdata.plot()
-
-
-totalvalue = sum(portfolio.values())
-print(totalvalue)
-totalvalue.plot()
 plt.title('Portfolio')
-list_legend = list(portfolio.keys())
-list_legend.append("Total value")
-plt.legend(list_legend)
 plt.show()
+
+
+# fig = make_subplots(rows=2, cols=1, vertical_spacing=0.03, specs=[[{"type": "table"}],[{"type": "scatter"}]])
+#
+# fig.add_trace(go.Scatter(x=sortdata["MSFT_x"],mode="lines",),row=2, col=1)
+#
+# fig.add_trace(
+# go.Table(
+#     header=dict(values=['MSFT'],
+#                 line_color='darkslategray',
+#                 fill_color='lightskyblue',
+#                 align='left'),
+#     cells=dict(values=[sortdata.MSFT_x],
+#                line_color='darkslategray',
+#                fill_color='lightcyan',
+#                align='left')),
+#     row=1, col=1
+# )
+#
+# fig.update_layout(
+#     height=500,
+#     width = 500,
+#     showlegend=False,
+#     title_text="Portfolio",
+# )
+#
+# fig.show()
