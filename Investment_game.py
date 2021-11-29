a0525947102653a5f434c5d9aab3fa26c30c48b0
rboumans <rboumans@deloitte.nl>
2021-11-29 11:40:01 +0100

    created sell stocks function

--- Investment_game.py
+++ Investment_game.py
@@ -1,8 +1,8 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+# import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
 
 
 def cash_balance():
@@ -21,6 +21,39 @@ def cash_balance():
             print("Amount not found, please fill in a number")
             continue
     return cash_balance
+
+def sellstocks():
+    while True:
+        ticker=input("Which stock from your portfolio would you like to sell? ")
+        if ticker in list(portfolio_aantal_stock.columns):
+            print("You have", int(portfolio_aantal_stock[ticker]) , "stocks in your portfolio")
+            while True:
+                try:
+                    sell=int(input("How many stocks would you like to sell? "))
+                    if sell > int(portfolio_aantal_stock[ticker]):
+                        print("You don't have this amount of stocks")
+                        continue
+                    elif sell <= 0:
+                        print("Please fill in a positive number ")
+                        continue
+                    else:
+                        break
+                except:
+                    print("Please fill in a number ")
+                    continue
+        else:
+            decision=input("We could not find this ticker in your portfolio, would you like to try again [y/n]? ")
+            if decision == "y":
+                continue
+            else:
+                break
+        sellstock2=input("Would you like to sell another stock [y/n]? ")
+        if sellstock2 == "y":
+            continue
+        else:
+            break
+
+
 
 portfolio= pd.DataFrame()
 portfolio_per_stock=pd.DataFrame()
@@ -77,6 +110,7 @@ def GetPrices():
         else:
             balance -= numberofstocks * purchase_price
             print("your remaining balance is:", balance)
+
         answer = input("Want to continue [y/n]? ")
         if answer == 'y':
             continue
@@ -87,43 +121,46 @@ def GetPrices():
 stock_price=GetPrices()
 data = pd.merge(portfolio,portfolio_per_stock, left_index=True, right_index=True)
 sortdata = data.sort_index(ascending=True)
+print(sortdata)
+print(portfolio_aantal_stock)
+sellstocks()
 
+#
+#
+# # sortdata.plot()
+# # plt.title('Portfolio')
+# # plt.show()
+#
+# fig = make_subplots(
+#     rows=2, cols=1,
+#     vertical_spacing=0.03,
+#     specs=[[{"type": "table"}],
+#            [{"type": "scatter"}]]
+# )
 
-
-# sortdata.plot()
-# plt.title('Portfolio')
-# plt.show()
-
-fig = make_subplots(
-    rows=2, cols=1,
-    vertical_spacing=0.03,
-    specs=[[{"type": "table"}],
-           [{"type": "scatter"}]]
-)
-
-
-for i in range(0, len(portfolio.columns)):
-    fig.add_trace(
-        go.Scatter(x= portfolio.index, y=portfolio.iloc[:,i],
-                        mode='lines', name=portfolio.columns[i]),
-        row=2, col=1
-    )
-
-
-for i in range(0, len(portfolio_per_stock.columns)):
-    fig.add_trace(
-        go.Table(
-        header=dict(values=["Stocks in portfolio", "Number of Stock in portfolio", "Price per stock", "Total Value"],
-                    line_color='darkslategray',
-                    fill_color='lightskyblue',
-                    align='left'),
-        cells=dict(values=[portfolio_per_stock.columns, list(portfolio_aantal_stock.iloc[0,:]), list(portfolio_per_stock.iloc[0,:]), list(portfolio.iloc[0,:])],
-                   line_color='darkslategray',
-                   fill_color='lightcyan',
-                   align='left')),
-        row=1, col=1
-    )
-
-fig.show()
+#
+# for i in range(0, len(portfolio.columns)):
+#     fig.add_trace(
+#         go.Scatter(x= portfolio.index, y=portfolio.iloc[:,i],
+#                         mode='lines', name=portfolio.columns[i]),
+#         row=2, col=1
+#     )
+#
+#
+# for i in range(0, len(portfolio_per_stock.columns)):
+#     fig.add_trace(
+#         go.Table(
+#         header=dict(values=["Stocks in portfolio", "Number of Stock in portfolio", "Price per stock", "Total Value"],
+#                     line_color='darkslategray',
+#                     fill_color='lightskyblue',
+#                     align='left'),
+#         cells=dict(values=[portfolio_per_stock.columns, list(portfolio_aantal_stock.iloc[0,:]), list(portfolio_per_stock.iloc[0,:]), list(portfolio.iloc[0,:])],
+#                    line_color='darkslategray',
+#                    fill_color='lightcyan',
+#                    align='left')),
+#         row=1, col=1
+#     )
+#
+# fig.show()
 
 
